78f2c463257c49d9cb058b9c5f8d8d7155f881a4
rajatparashar <rajatparasharan@gmail.com>
2020-12-05 16:39:47 +0530

    First commit

---
+++ AutomationTesting/src/test/java/qa/com/test/TestClass.java
@@ -0,0 +1,18 @@
+package qa.com.test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+public class TestClass {
+
+	@Test
+	public void googleSearchTest() throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com/");
+		driver.findElement(By.name("q")).sendKeys("ABC");		
+		Thread.sleep(2000);
+		driver.close();
+	}
+}
