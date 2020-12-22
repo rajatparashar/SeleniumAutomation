a2d9ca026b11a4a0acaf0ad732d903f00396626d
rajatparashar <rajatparasharan@gmail.com>
2020-12-22 15:43:12 +0530

    Added new classes

--- AutomationTesting/src/test/java/qa/com/test/TestClass.java
+++ AutomationTesting/src/test/java/qa/com/test/TestClass.java
@@ -6,13 +6,38 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 public class TestClass {
 
-	@Test
-	public void googleSearchTest() throws InterruptedException {
-		WebDriver driver = new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.get("https://www.google.com/");
-		driver.findElement(By.name("q")).sendKeys("ABC");		
-		Thread.sleep(2000);
-		driver.close();
-	}
+//	@Test
+//	public void googleSearchTest() throws InterruptedException {
+//		WebDriver driver = new ChromeDriver();
+//		driver.manage().window().maximize();
+//		driver.get("https://www.google.com/");
+//		driver.findElement(By.name("q")).sendKeys("ABC");		
+//		Thread.sleep(2000);
+//		driver.close();
+//	}
+//	
+	
+	
+	
+
+public static void main(String[] args) {
+
+int input = 123456;
+int remainder = 0, reverse = 0;
+
+while(input > 0) {
+
+remainder = input%10;
+input = input/10;
+reverse = reverse*10+remainder;
+
+}
+
+System.out.println(reverse);
+
+}
+
+
+
+	
 }
